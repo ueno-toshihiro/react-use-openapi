7bf806a2e53f259f1a56ca91bf5d7f9f04f2246e
ueno <ueno.hub@gmail.com>
2024-07-25 11:02:52 +0900

    orval.config の入力・出力ファイル設定を変更
    参照
    https://orval.dev/reference/configuration/overview

--- orval.config.js
+++ orval.config.js
@@ -1,6 +1,20 @@
 module.exports = {
-  "petstore-file": {
-    input: "./schema/openapi.yaml",
-    output: "./src/users.ts",
+  "users-store-file-transformer": {
+    input: {
+      target: "./schema/openapi.yml",
+      validation: true,
+    },
+    output: {
+      mode: "split",
+      target: "./src/gen/users.ts",
+      schema: "./src/gen/models",
+      client: "react-query",
+      httpClient: "fetch",
+      baseUrl: "http://localhost:3000",
+      mock: true,
+    },
+    hooks: {
+      afterAllFilesWrite: "prettier --write",
+    },
   },
 };
